8f84b6717464a16a040c26c4efeec3b776ff783d
Iwajaki <iwajaki1994220@gmail.com>
2020-01-15 10:56:35 +0800

    revise the store.js  gallery part revise

--- assets/js/store.js
+++ assets/js/store.js
@@ -4077,7 +4077,34 @@ jQuery(document).ready(function() {
 		} else {
 			$(".areas").addClass("area-hidden");
 		}
-	});	
+	});
+
+    if ( $(".food-gallery-wrap").exists()){
+          $('.food-gallery-wrap').magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          closeOnContentClick: false,
+          closeBtnInside: false,
+          mainClass: 'mfp-with-zoom mfp-img-mobile',
+          image: {
+            verticalFit: true,
+            titleSrc: function(item) {
+              return '';
+            }
+          },
+          gallery: {
+            enabled: true
+          },
+          zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+              return element.find('img');
+            }
+          }      
+        });
+              
+    }
 		
 }); /*end ready*/
 /** END ADDED CODE VERSION 2.6*/
